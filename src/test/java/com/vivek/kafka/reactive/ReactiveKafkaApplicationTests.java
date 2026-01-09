655d728d5f09524cf49c60e04fc2f3b41257b9b7
Vivek C <vivekchinchole98@gmail.com>
2026-01-09 21:14:26 +0530

    Initial commit: Reactive Kafka WebFlux application

---
+++ src/test/java/com/vivek/kafka/reactive/ReactiveKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.kafka.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
